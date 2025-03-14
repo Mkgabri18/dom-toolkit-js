b4492d5241e8185614a1385d581364eaf593885d
mkgabri18 <carrasco.gabriel136@hotmail.it>
2025-03-14 08:17:03 +0100

    export in index.js, use main.js to html

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
-import ClassList from './src/classList.js';
-// import { onEvent } from './listenerEvent.js';
-/* import { selectId } from './src/selectors.js'; */
 import Selectors from './src/selectors.js';
-import { local, session } from './src/storage.js'
+import { local, session } from './src/storage.js';
+import indexOf from './src/indexOf.js';
+import { onEvent, onEventOnce, offEvent } from './src/listenerEvent.js';
+import ClassList from './src/classList.js';
 
-const { selectId } = new Selectors()
-
-// onEvent($button, 'click', changecolor);
-selectId('addColor').onClick(addChangecolor);
-selectId('toggleColor').onClick(toggleChangecolor);
-selectId('removeColor').onClick(removeChangecolor);
-selectId('replacecolor').onClick(replacecolor);
-
-console.log("not found elem. ", selectId(''))
-// selectId('addColors')?.onClick(addChangecolor);
-
-function addChangecolor() {
-    selectId('description').addClass('color-red font-xl');
-}
-
-function toggleChangecolor() {
-    selectId('description').toggleClass('color-red');
-}
-
-function removeChangecolor() {
-    selectId('description').removeClass('color-red font-xl');
-}
-
-function replacecolor() {
-    selectId('description').replaceClass('color-red', 'color-blue');
-}
-
-// Try localStorage tility funciton
-local.add('developer', {name: 'Gabriel', age: 33})
-session.add('salary', 46000)
-
-const $span = selectId('dev')
-
-if ($span) {
-    const developer = local.get('developer')
-    const salary = session.get('salary')
-    $span.textContent = `
-        Nome: ${developer.name},
-        Age: ${developer.age},
-        Salary: ${salary}
-    `
+export {
+    Selectors,
+    local,
+    session,
+    indexOf,
+    onEvent,
+    onEventOnce,
+    offEvent,
+    ClassList
 }
---
+++ main.js
@@ -0,0 +1,45 @@
+import { Selectors } from './index.js';
+import { local, session } from './index.js';
+
+const { selectId } = new Selectors()
+
+// onEvent($button, 'click', changecolor);
+selectId('addColor').onClick(addChangecolor);
+selectId('toggleColor').onClick(toggleChangecolor);
+selectId('removeColor').onClick(removeChangecolor);
+selectId('replacecolor').onClick(replacecolor);
+
+console.log("not found elem. ", selectId(''))
+// selectId('addColors')?.onClick(addChangecolor);
+
+function addChangecolor() {
+    selectId('description').addClass('color-red font-xl');
+}
+
+function toggleChangecolor() {
+    selectId('description').toggleClass('color-red');
+}
+
+function removeChangecolor() {
+    selectId('description').removeClass('color-red font-xl');
+}
+
+function replacecolor() {
+    selectId('description').replaceClass('color-red', 'color-blue');
+}
+
+// Try localStorage tility funciton
+local.add('developer', {name: 'Gabriel', age: 33})
+session.add('salary', 46000)
+
+const $span = selectId('dev')
+
+if ($span) {
+    const developer = local.get('developer')
+    const salary = session.get('salary')
+    $span.textContent = `
+        Nome: ${developer.name},
+        Age: ${developer.age},
+        Salary: ${salary}
+    `
+}
